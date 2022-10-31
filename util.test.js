06d906d547145aae8edb9f61d9b3bf27f0d324ac
nawodyaishan <nawodyain@gmail.com>
2022-10-31 17:22:41 +0530

    Add browser test

--- util.test.js
+++ util.test.js
@@ -1,4 +1,5 @@
 const {generateText, checkAndGenerate} = require(`./util`);
+const puppeteer = require(`puppeteer`);
 
 test(`Should output name and age`, () => {
     let text = generateText(`Max`, 29);
@@ -13,3 +14,23 @@ test(`Should generate validate text output`, () => {
     let text = checkAndGenerate(`Max`, 29)
     expect(text).toBe(`Max (29 years old)`)
 })
+
+test(`should click around `, async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 220,
+        args: [`--window-size=1920,1080`]
+    })
+
+    const page = await browser.newPage()
+    await page.goto(`file:///Users/nawodyaishan/Documents/GitHub/WebDev-Js-Unit-Test-Jest-Demo/index.html`);
+
+    await page.click(`input#name`);
+    await page.type(`input#name`, `Anna`);
+    await page.click(`input#age`);
+    await page.type(`input#age`, `37`);
+    await page.click(`#btnAddUser`);
+
+    let finalText = await page.$eval(`.user-item`, el => el.textContent)
+    expect(finalText).toBe(`Anna (37 years old)`);
+});
